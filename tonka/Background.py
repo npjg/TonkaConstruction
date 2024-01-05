932671e92adc6f69cb92bf069dfabb55ed149904
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-01-04 20:08:20 -0500

    Fix mismatch with assets framework.

--- tonka/Background.py
+++ tonka/Background.py
@@ -7,19 +7,13 @@ from asset_extraction_framework.Asset.Palette import RgbPalette
 class Background(RectangularBitmap):
     def __init__(self, file):
         super().__init__()
-
         self.name = "Background"
-
-        # TODO: Unknown data.
         self.unk1 = struct.unpack.uint16_le(file.stream)
         self.unk2 = struct.unpack.uint16_le(file.stream)
         self.unk3 = struct.unpack.uint16_le(file.stream)
         self.unk4 = struct.unpack.uint16_le(file.stream)
         self.unk5 = struct.unpack.uint16_le(file.stream)
-
         self.filename = file.stream.read(0x50).rstrip(b'\x00')
-
-        # TODO: Unknown data.
         self.unk6 = struct.unpack.uint32_le(file.stream)
         self.unk7 = struct.unpack.uint32_le(file.stream)
         self.unk8 = struct.unpack.uint32_le(file.stream)
@@ -28,12 +22,10 @@ class Background(RectangularBitmap):
         self.unk9 = struct.unpack.uint32_le(file.stream)
         self.unk10 = struct.unpack.uint32_le(file.stream)
         self.unk13 = file.stream.read(0x14)
-        self._palette = RgbPalette(file, has_entry_alignment = True, total_palette_entries = 0x100, blue_green_red_order = True)
+        self._palette = RgbPalette(file.stream, has_entry_alignment = True, total_palette_entries = 0x100, blue_green_red_order = True)
         self._width = struct.unpack.uint32_le(file.stream)
         self._height = struct.unpack.uint32_le(file.stream)
         pixel_count = struct.unpack.uint32_le(file.stream)
-
-        # TODO: Unknown data.
         self.unk14 = file.stream.read(0x0c)
 
         # The image data is always uncompressed.
