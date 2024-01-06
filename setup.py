5584e4d680aec017130df835c9e715778c7a7817
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-01-06 09:38:04 -0500

    Move source files to package-named folder.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 
 # For development work, looks like you would use this to compile:
 #  python3 setup.py build_ext --inplace
-packbits = Extension('PackBits', sources = ['tonka/PackBits.c'])
+packbits = Extension('PackBits', sources = ['TonkaConstruction/PackBits.c'])
 setup(
     name = 'Tonka',
     version = '0.1',
