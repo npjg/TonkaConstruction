08859b3b4f95d550254bac414f9a77c46cffff0a
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-01-10 21:41:11 -0500

    Fix typos in test script.

--- tests/test_game_parsing.py
+++ tests/test_game_parsing.py
@@ -7,11 +7,6 @@ import pytest
 
 from TonkaConstruction import Engine 
 
-# This package includes a command that can be called from the command line,
-# so we will store the name of that script here. It is defined in pyproject.toml,
-# but I didn't see an easy way to reference that here, so we'll just hardcode it.
-CALLABLE_SCRIPT_NAME = 'TonkaConstruction'
-
 # The tests MUST be run from the root of the repository.
 GAME_ROOT_DIRECTORY = 'tests/test_data'
 game_directories = []
@@ -21,25 +16,30 @@ for filename in os.listdir(os.path.realpath(GAME_ROOT_DIRECTORY)):
         game_directories.append(filepath)
 
 def test_script_is_runnable():
-        # RUN THE PYPIX2SVG SCRIPT.
-        # We shell out rather than just calling the function from Python to make
-        # sure that the script entry point is installed correctly too.
-        # We don't need to actually process anything, just make sure the script runs.
-        # So we can point it to an empty directory.
-        empty_directory = tempfile.mkdtemp()
-        try:
-            # ATTEMPT TO RUN THE SCRIPT.
-            command = [CALLABLE_SCRIPT_NAME, empty_directory]
-            result = subprocess.run(command, capture_output = True, text = True)            
+    # This package includes a command that can be called from the command line,
+    # so we will store the name of that script here. It is defined in pyproject.toml,
+    # but I didn't see an easy way to reference that here, so we'll just hardcode it.
+    CALLABLE_SCRIPT_NAME = 'TonkaConstruction'
 
-            # VERIFY THE SCRIPT RAN SUCCESSFULLY.
-            if (result.returncode != 0):
-                raise AssertionError(
-                    f'Received a nonzero exit code when running `{CALLABLE_SCRIPT_NAME}` from command line!'
-                    f'\nstdout: {result.stdout}'
-                    f'\n\nstderr: {result.stderr}')
-        finally:
-            shutil.rmtree(empty_directory)
+    # RUN THE SCRIPT.
+    # We shell out rather than just calling the function from Python to make
+    # sure that the script entry point is installed correctly too.
+    # We don't need to actually process anything, just make sure the script runs.
+    # So we can point it to an empty directory.
+    empty_directory = tempfile.mkdtemp()
+    try:
+        # ATTEMPT TO RUN THE SCRIPT.
+        command = [CALLABLE_SCRIPT_NAME, empty_directory]
+        result = subprocess.run(command, capture_output = True, text = True)            
+
+        # VERIFY THE SCRIPT RAN SUCCESSFULLY.
+        if (result.returncode != 0):
+            raise AssertionError(
+                f'Received a nonzero exit code when running `{CALLABLE_SCRIPT_NAME}` from command line!'
+                f'\nstdout: {result.stdout}'
+                f'\n\nstderr: {result.stderr}')
+    finally:
+        shutil.rmtree(empty_directory)
 
 @pytest.mark.parametrize("game_directory_path", game_directories)
 def test_process_game(game_directory_path):
